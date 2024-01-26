ed06ece23383a09dc925f6070ce07015867a9344
Zubcal <127558095+Zubcal@users.noreply.github.com>
2024-01-26 02:38:48 +0100

    Update install.sh

--- monitoring-server/install.sh
+++ monitoring-server/install.sh
@@ -22,6 +22,7 @@ docker_installation() {
             jq
             ssmtp
             mailutils
+            inotify-tools
 
         # Install Docker
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
