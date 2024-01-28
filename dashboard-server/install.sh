503a4b06ccc4d89689179252a307d451bc56d34c
Zubcal <127558095+Zubcal@users.noreply.github.com>
2024-01-28 10:23:51 +0100

    Update install.sh

--- dashboard-server/install.sh
+++ dashboard-server/install.sh
@@ -71,10 +71,66 @@ create_folders() {
     mkdir -p /opt/M122/promtail
 
     # installieren von dem log firmatieren script
-    wget -O /opt/M122/scripts/log_formating.sh https://raw.githubusercontent.com/Zubcal/m122/main/monitoring-server/log_formating.sh > /dev/null 2>&1
+    wget -O /opt/M122/loki/loki-config.yaml https://raw.githubusercontent.com/Zubcal/m122/main/dashboard-server/loki-config.yaml > /dev/null 2>&1
 
-    sudo chmod +x /opt/M122/scripts/log_formating.sh
+    # installieren von dem log firmatieren script
+    wget -O /opt/M122/promtail/promtail-config.yaml https://raw.githubusercontent.com/Zubcal/m122/main/dashboard-server/promtail-config.yaml > /dev/null 2>&1
     
-    # Service file herunterladen
-    wget -O /etc/systemd/system/log_formating.service https://raw.githubusercontent.com/Zubcal/m122/main/monitoring-server/log_formating.service > /dev/null 2>&1
+    # installieren von dem log firmatieren script
+    wget -O /opt/M122/docker/docker-compose.yaml https://raw.githubusercontent.com/Zubcal/m122/main/dashboard-server/docker-compose.yaml > /dev/null 2>&1
 }
+
+install_syncthing() {
+
+        # Überprüfen, ob das Docker-Netzwerk namens "nextcloud" existiert
+    if ! sudo docker network inspect nextcloud &> /dev/null; then
+        # Das Netzwerk existiert nicht, erstelle es
+        sudo docker network create nextcloud
+    fi
+
+    # Run docker-compose up -d in /opt/M122/docker
+    sudo docker-compose -f /opt/M122/docker/docker-compose.yaml up -d
+
+    echo "Syncthing wird installiert. Bitte warten..."
+    sleep 30  # Warte 30 Sekunden
+
+    # Zeige die Syncthing-Gerät-ID
+    current_device_id=$(sudo docker exec -t syncthing syncthing --device-id)
+    echo "❕ Dies ist Syncthing-Gerät-ID  bitte füge diese ID in ihrem Monitoring Sycthing ein: $current_device_id"
+
+
+    # Benutzer nach der ID des Syncthing vom Monotoring fragen und Überprüfung der Eingabe
+    while true; do
+        read -p "❕ Geben Sie die ID Ihres Monitoring für Syncthing ein: " monitoring_id </dev/tty
+
+        # Überprüfen, ob die ID dem gewünschten Schema entspricht
+        if [[ ! "$monitoring_id" =~ ^[A-Z0-9-]+-[A-Z0-9-]+-[A-Z0-9-]+-[A-Z0-9-]+-[A-Z0-9-]+-[A-Z0-9-]+-[A-Z0-9-]+-[A-Z0-9-]+$ ]]; then
+            sudo echo "Ungültige Eingabe. Bitte geben Sie eine ID im richtigen Format ein❗️"
+        else
+            break
+        fi
+    done
+
+
+    sudo docker exec -t syncthing syncthing cli config devices add --device-id "$monitoring_id" --name monitoring --addresses dynamic --auto-accept-folders
+    echo "Gerät 'Monitoring' wurde zu Syncthing hinzugefügt. ✅ "
+
+        # Überprüfen, ob Gerät 'Dashboard' bereits mit dem Ordner 'Monitoring' geteilt ist
+    if ! sudo docker exec -t syncthing syncthing cli config folders "$syncthing_folder_check" devices list | grep -q "$dashboard_id"; then
+        # Gerät 'Dashboard' zum Ordner 'Monitoring' hinzufügen
+        sudo docker exec -t syncthing syncthing cli config folders "$syncthing_folder_check" devices add --device-id "$dashboard_id"
+        echo "Gerät 'Dashboard' wurde zum Ordner 'Monitoring' hinzugefügt. ✅"
+    else
+        echo "Gerät 'Dashboard' ist bereits mit dem Ordner 'Monitoring' geteilt. ✅"
+    fi
+
+}
+
+# Schritt 1: Installation von Docker und Docker Compose
+docker_installation
+
+# Schritt 2: Erstellen von Ordnern für Syncthing und Scripts
+create_folders
+
+# Schritt 3: Installieren von Syncthing mit Docker Compose
+install_syncthing
